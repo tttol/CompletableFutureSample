1aef5289b4c94416f363cf86c303f51e50bae56e
t-takahashi <tkhstol.929@gmail.com>
2022-12-18 23:00:54 +0900

    refactor

---
+++ src/main/java/CompletableFutureJob.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+public class CompletableFutureJob {
+    private final Logger log = Logger.getLogger("Run");
+    /**
+     * CompletableFutureを使った単発処理
+     * @throws Exception
+     */
+    public void doCompletableFuture(int initialValue) throws Exception {
+        log.info("[START] doAsyncTask");
+        log.info("initial value = [%d]".formatted(initialValue));
+
+        Supplier<Integer> getIntVal = () -> initialValue;
+        Function<Integer, Integer> func = value -> {
+            try {
+                log.info("[START]重い処理");
+                for (int i = 0; i < 3; i++) {
+                    Thread.sleep(1000);
+                    log.info("%d秒経過".formatted(i + 1));
+                }
+            } catch (InterruptedException e) {
+                log.warning("割り込み例外");
+            } finally {
+                log.info("[END]重い処理");
+            }
+            return value;
+        };
+        Consumer<Integer> out = value -> log.info("value=%d".formatted(value));
+
+        var future = CompletableFuture.supplyAsync(getIntVal)
+                .thenApply(func)
+                .thenAccept(out);
+
+        // future.get()をするとfuncの重い処理が終わるまで待つ。future.get()がない場合は待たない。
+        future.get();
+
+        log.info("[END] doAsyncTask");
+    }
+
+    /**
+     * CompletableFutureを使った複数処理
+     * @throws Exception
+     */
+    public void doMultiCompletableFuture() throws Exception {
+        log.info("[START] doMultiCompletableFuture");
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int n = i;
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> log.info("task-%d is executed".formatted(n)));
+            futures.add(future);
+        }
+        // .allOf().get()をすると処理が終わるまで待つ。.allOf().get()ない場合は待たない。
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[] {})).get();
+        log.info("[END] doMultiCompletableFuture");
+    }
+}
--- src/main/java/CompletableFutureSampleMain.java
+++ src/main/java/CompletableFutureSampleMain.java
@@ -9,10 +9,10 @@ public class CompletableFutureSampleMain {
         System.setProperty("java.util.logging.SimpleFormatter.format",
                 "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$s %3$s : %5$s%6$s%n");
 
-        var asyncJob = new AsyncJob();
+        var completableFutureJob = new CompletableFutureJob();
         try {
-            asyncJob.doCompletableFuture();
-            asyncJob.doMultiCompletableFuture();
+            completableFutureJob.doCompletableFuture((int) Math.floor(Math.random() * 10));
+            completableFutureJob.doMultiCompletableFuture();
         } catch (Exception e) {
             log.warning(e.getMessage());
         }
